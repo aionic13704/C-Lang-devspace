58eb6aea0b8e33960772edafdf06fb83a860e521
aionic13704 <aionic13704@gmail.com>
2020-12-19 15:41:51 +0000

    admin으로 로그인한 경우 디자인

--- main.c
+++ main.c
@@ -8,10 +8,12 @@ int isAdmin();      // admin인지 아닌지 판단
 int insert_Money(); // 돈을 넣는 것을 담당함
 int welcome_menu(); // 처음 맞이하는 화면
 int show_List(int p);  // 사용자에 따라 다른 화면을 보여줌
+int withdraw();       // 출금을 담당하는 함수
 
 // 각 음료의 가격
 const int cola = 900, cider = 900, pocari = 700, sparking = 800, juice = 600, dew = 800;
-int total_money = 0;    // 보유중인 금액
+int cola_c = 10, cider_c = 5, pocari_c = 23, sparking_c = 17, juice_c = 22, dew_c = 8;
+int total_money = 5000;    // 보유중인 금액
 
 int main(){
   int select = welcome_menu();
@@ -66,8 +68,10 @@ int welcome_menu(){   // 처음 맞이하는 화면
   }
 }
 int show_List(int p){    // 사용자에 따라 다른 화면을 보여줌
-  int select_ad, select_cu;
+  int select_ad, select_cu, select_mn;
   int needs_money, ins_money, return_money;
+  int with_mon;
+
   if(p == 0){     // 관리자 메뉴
     printf("=======Admin Menu=======\n\n");
 
@@ -75,7 +79,26 @@ int show_List(int p){    // 사용자에 따라 다른 화면을 보여줌
     printf("1. Withdraw  2. Refill\n");
     scanf("%d", &select_ad);
 
-    if(select_ad != 1 || select_ad != 2){
+    if(select_ad == 1){
+      printf("Current balance : %dwon\n", total_money);
+      with_mon = withdraw();
+      total_money -= with_mon;
+
+      printf("Success withdraw! Balance : %d\n", total_money);
+    }
+
+    else if(select_ad == 2){
+      if(show_Menu(0) != 0){
+        printf("!Success refill!\n====Stock====\nCoka Cola : %d\tPocari Sweat : %d\tMountain Dew : %d\ncider : %d\t   Grape Juice : %d   Sparking : %d\n\n", cola_c, pocari_c, dew_c, cider_c, juice_c, sparking_c);
+      }
+
+      else{
+        printf("Error!\n");
+      }
+
+    }
+
+    else{
       printf("!Insert Error!\nPlz insert exactly\n");
       goto back_ad;
     }
@@ -83,45 +106,7 @@ int show_List(int p){    // 사용자에 따라 다른 화면을 보여줌
 
   else{   // 고객 메뉴
     printf("=======Customer Menu=======\n\n");
-
-    back_cu:
-    printf("1. Coka Cola  2. Pocari Sweat  3. Mountain Dew\n4. cider\t  5. Grape Juice   6. Sparking\n\n");
-    scanf("%d", &select_cu);
-
-    if(select_cu < 1 || select_cu > 6){
-      printf("!Insert Error!\nPlz insert exactly\n");
-      goto back_cu;
-    }
-
-    switch(select_cu){
-      case 1:
-        needs_money = cola;
-        break;
-
-      case 2:
-        needs_money = pocari;
-        break;
-
-      case 3:
-        needs_money = dew;
-        break;
-
-      case 4:
-        needs_money = cider;
-        break;
-
-      case 5:
-        needs_money = juice;
-        break;
-
-      case 6:
-        needs_money = sparking;
-        break;
-
-      default:
-        printf("Error\n");
-        break;
-    }
+    needs_money = show_Menu(1);
 
     printf("You can insert %dwon to drink!\n", needs_money);
 
@@ -142,6 +127,99 @@ int show_List(int p){    // 사용자에 따라 다른 화면을 보여줌
   return 0;
 }
 
-void withdraw(){
-  
+int withdraw(){ // 출금을 담당하는 함수
+  int money;
+  printf("Enter money : ");
+  scanf("%d", &money);
+
+  back:
+  if(money > total_money){
+    printf("!limit Excess!\nPlz insert exactly\n");
+    goto back;
+  }
+
+  return money;
+}
+
+int show_Menu(int p){
+  int select, n, count;
+  printf("1. Coka Cola  2. Pocari Sweat  3. Mountain Dew\n4. cider\t  5. Grape Juice   6. Sparking\n\n");
+  scanf("%d", &select);
+
+  back_cu:
+  if(select < 1 || select > 6){
+    printf("!Insert Error!\nPlz insert exactly\n");
+    goto back_cu;
+  }
+
+  if(p == 1){
+    switch(select){
+      case 1:
+        n = cola;
+        break;
+
+      case 2:
+        n = pocari;
+        break;
+
+      case 3:
+        n = dew;
+        break;
+
+      case 4:
+        n = cider;
+        break;
+
+      case 5:
+        n = juice;
+        break;
+
+      case 6:
+        n = sparking;
+        break;
+
+      default:
+        printf("Error\n");
+        break;
+    }
+
+    return n;
+  }
+
+  else{
+    printf("Enter integer : ");
+    scanf("%d", &count);
+
+    switch(select){
+      case 1:
+        cola_c += count;
+        break;
+
+      case 2:
+        pocari_c += count;
+        break;
+
+      case 3:
+        dew_c += count;
+        break;
+
+      case 4:
+        cider_c += count;
+        break;
+
+      case 5:
+        juice_c += count;
+        break;
+
+      case 6:
+        sparking_c += count;
+        break;
+
+      default:
+        printf("Error\n");
+        break;
+    }
+  }
+
+  return 1;
 }
